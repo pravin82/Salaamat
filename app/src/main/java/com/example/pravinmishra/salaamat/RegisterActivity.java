7383106d6ec50b73516509c4281812499764ff4e
pravinmishra <pravinmishra1796@gmail.com>
2017-12-02 10:24:09 +0530

    Make Main_Activity(Login)

---
+++ app/src/main/java/com/example/pravinmishra/salaamat/RegisterActivity.java
@@ -0,0 +1,11 @@
+package com.example.pravinmishra.salaamat;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pravinmishra on 02/12/17.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+}
